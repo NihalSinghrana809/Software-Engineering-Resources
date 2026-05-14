7d1611aec66105de15e40a2552ab2c3a58657f44
Nihal Singh Rana <nihalsinghrana809@gmail.com>
2026-05-14 09:00:44 +0530

    Add pattern printing program in Pattern-01.cpp
    Implement a pattern printing program that outputs a square of numbers based on user input.

---
+++ DSA/Patterns/Pattern-01.cpp
@@ -0,0 +1,21 @@
+// Pattern-01
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+      int k=1;
+      for(int j=1;j<=n;j++){
+            cout<<k;
+            k++;
+      }
+      cout<<endl;
+    }
+    return 0;
+}
