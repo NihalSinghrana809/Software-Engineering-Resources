9ff269e5eff1bf18327a4f3f0fb83f9b87fdd0d0
Nihal Singh Rana <nihalsinghrana809@gmail.com>
2026-05-16 07:04:46 +0530

    Create Pattern-05.cpp

---
+++ DSA/Patterns/Pattern-05.cpp
@@ -0,0 +1,19 @@
+// Pattern-05 (Pascal's Triangle)
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+// 1 4 6 4 1
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        int num=1;
+        for(int j=0;j<=i;j++){
+            cout<<num<<" ";
+            num=num*(i-j)/(j+1);
+        }
+        cout << endl;
+    }
+}
