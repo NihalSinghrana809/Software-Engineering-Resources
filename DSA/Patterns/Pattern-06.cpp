896b992c49055e3fd77eb6e342e5695f56a21972
Nihal Singh Rana <nihalsinghrana809@gmail.com>
2026-05-17 15:54:40 +0530

    Update Pattern-06.cpp

--- DSA/Patterns/Pattern-06.cpp
+++ DSA/Patterns/Pattern-06.cpp
@@ -8,9 +8,11 @@ int main(){
       int n;
       cin>>n;
       for(int i=1;i<=n;i++){
+            // Spaces
             for(int j=1;j<=n-i;j++){
                   cout<<" ";
             }
+            // Stars
             for(int j=1;j<=2*i-1;j++){
                   cout<<"*";
             }
