3787a4b4e1e8979d243b16880ab4181b365aaeed
Nihal Singh Rana <nihalsinghrana809@gmail.com>
2026-05-20 10:18:52 +0530

    Add files via upload

---
+++ DSA/Patterns/Pattern-08.cpp
@@ -0,0 +1,32 @@
+// Pattern-08
+//   *
+//  ***
+//   *
+#include<iostream>
+using namespace std;
+int main(){
+      int n;
+      cin>>n;
+      for(int i=1;i<=n;i++){
+            // Spaces
+            for(int j=1;j<=n-i;j++){
+                  cout<<" ";
+            }
+            // Stars
+            for(int j=1;j<=2*i-1;j++){
+                  cout<<"*";
+            }
+            cout<<endl;
+      }
+            for(int i=n-1;i>=1;i--){
+            // Spaces
+            for(int j=1;j<=n-i;j++){
+                  cout<<" ";
+            }
+            // Stars
+            for(int j=1;j<=2*i-1;j++){
+                  cout<<"*";
+            }
+            cout<<endl;
+      }
+}
